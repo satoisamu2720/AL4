f5a2e06d0cf4a4cf5f9c5ed2824db45b07ad04cf
功夢 佐藤 <k022g0117@g.neec.ac.jp>
2023-05-10 14:22:41 +0900

    初期化

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,80 +1,33 @@
 #include "GameScene.h"
 #include "TextureManager.h"
-#include "ImGuiManager.h"
-#include "PrimitiveDrawer.h"
-#include "AxisIndicator.h"
 #include <cassert>
-
-
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() { 
-	delete sprite_;
-	delete model_;
-	delete debugCamera_;
-}
+GameScene::~GameScene() {}
 
 void GameScene::Initialize() {
-	// ファイル名を指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("sample.png");
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
-	model_ =  Model::Create();
-	worldTransform_.Initialize();
-	viewProjection_.Initialize();
-	soundDataHandle_ = audio_->LoadWave("fanfare.wav");
-	audio_->PlayWave(soundDataHandle_);
-	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
-	//ライン描画が参照するビュープロジェクションを指定する（アドレス渡し）
-	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
-	debugCamera_ = new DebugCamera(1280, 720);
-	//軸方向表示の表示を有効にする
-	AxisIndicator::GetInstance()->SetVisible(true);
-	//軸方向表示が参照するビュープロジェクションを指定する（アドレス渡し）
-	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
-
 }
 
-void GameScene::Update() {
-
-	//スプライトの今の座標を取得
-	Vector2 position = sprite_->GetPosition();
-	//座標を｛２，１｝に移動
-	position.x += 2.0f;
-	position.y += 1.0f;
-	//移動した座標をスプライトに反射
-	sprite_->SetPosition(position);
-	if (input_->TriggerKey(DIK_SPACE)) {
-		audio_->StopWave(voiceHandle_);
-	}
-	ImGui::Begin("Debug1");
-	// float3入力ボックス
-	ImGui::InputFloat3("InputFloat3", inputFloat3);
-	// Float3スライダー
-	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
-	ImGui::Text("kamata Tarou %d.%d.%d,2050,12,31");
-	ImGui::End();
-	ImGui::ShowDemoWindow();
-	debugCamera_->Update();
-}
+void GameScene::Update() {}
 
 void GameScene::Draw() {
-	
+
 	// コマンドリストの取得
 	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
 
 #pragma region 背景スプライト描画
 	// 背景スプライト描画前処理
 	Sprite::PreDraw(commandList);
-	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0,20,0}, {1.0f, 0.0f,0.0f, 1.0f});
-	
+
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
-	
+
 	// スプライト描画後処理
 	Sprite::PostDraw();
 	// 深度バッファクリア
@@ -88,8 +41,6 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-	//model_->Draw(worldTransform_, viewProjection_, textureHandle_);
-	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -102,7 +53,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	//sprite_->Draw();
+
 	// スプライト描画後処理
 	Sprite::PostDraw();
 
