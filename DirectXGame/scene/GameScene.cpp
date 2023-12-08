97c0a6e925cd7c86daa526459cc81c2aec80f12c
satoisamu2720 <115054959+satoisamu2720@users.noreply.github.com>
2023-12-08 14:14:21 +0900

    ok

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -66,7 +66,7 @@ void GameScene::Initialize() {
 	followCamera_->SetTarget(&player_->GetWorldTransform());
 
 
-	player_->(&followCamera_->GetViewProjection());
+	player_->SetViewProjection(&followCamera_->GetViewProjection());
 	enemy_->SetViewProjection(&followCamera_->GetEnemyViewProjection());
 
 	
